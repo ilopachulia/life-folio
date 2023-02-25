ca4f3bf2b896072f18149437b10daff0dd71efb6
Ilia Pachulia <ipachulia@evolution.com>
2023-02-25 04:02:42 +0400

    card component was added and displayed dummy cards

---
+++ src/helper/project-list.jsx
@@ -0,0 +1,31 @@
+export const DUMMY_PROJECTS = [
+  {
+    id: 1,
+    title: "First",
+    description: "GREAT PROJECT FOR BEGINNERS",
+    img: "../../../assets/profilepic.png",
+  },
+  {
+    id: 2,
+    title: "First",
+    description: "GREAT PROJECT FOR BEGINNERS",
+    img: "../../../assets/profilepic.png",
+  },
+  {
+    id: 7,
+    title: "First",
+    description: "GREAT PROJECT FOR BEGINNERS",
+  },
+  {
+    id: 3,
+    title: "First",
+    description: "GREAT PROJECT FOR BEGINNERS",
+    img: "../../../assets/profilepic.png",
+  },
+  {
+    id: 4,
+    title: "First",
+    description: "GREAT PROJECT FOR BEGINNERS",
+    img: "../../../assets/profilepic.png",
+  },
+];
--- src/routes/pages/projects/projects.component.jsx
+++ src/routes/pages/projects/projects.component.jsx
@@ -1,5 +1,15 @@
+import classes from "./projects.module.scss";
+import Card from "./card/card.component";
+import { DUMMY_PROJECTS } from "../../../helper/project-list";
+
 const Projects = () => {
-  return <h1>I am a Projects Page</h1>;
+  return (
+    <div className={classes.projects_container}>
+      {DUMMY_PROJECTS.map((project) => {
+        return <Card key={project.id} project={project} />;
+      })}
+    </div>
+  );
 };
 
 export default Projects;
