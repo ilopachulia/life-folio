eb988531e05a628791d3dee1ca674057ed06085b
Ilia Pachulia <ipachulia@evolution.com>
2023-03-02 00:43:49 +0400

    contact page was styled

--- src/routes/pages/about/about.component.jsx
+++
@@ -1,5 +0,0 @@
-const About = () => {
-  return <h1>I am a About Page</h1>;
-};
-
-export default About;
--- src/routes/pages/contact/contact.component.jsx
+++ src/routes/pages/contact/contact.component.jsx
@@ -1,5 +1,48 @@
+import classes from "./contact.module.scss";
+import { BsPhoneVibrateFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FaFacebookSquare, FaLinkedin, FaGithub } from "react-icons/fa";
+
 const Contact = () => {
-  return <h1>I am a Contact Page</h1>;
+  return (
+    <div className={classes.contact_container}>
+      <div className={classes.sub_container}>
+        <span>
+          <BsPhoneVibrateFill />
+        </span>
+        +995 592 102 793
+      </div>
+      <div className={classes.sub_container}>
+        <span>
+          <MdEmail />
+        </span>
+        il.pachulia@gmail.com
+      </div>
+      <div className={classes.social_page_container}>
+        <a
+          href="https://www.linkedin.com/in/iliapachulia/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/Makata1962"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.facebook.com/ilo.pachulia98/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookSquare />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
