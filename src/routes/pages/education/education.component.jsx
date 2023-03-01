cb94842274d43fb71ec9c57c636bcf5f2f413352
Ilia Pachulia <ipachulia@evolution.com>
2023-03-01 23:38:46 +0400

    starting working on education component styling

--- src/routes/pages/education/education.component.jsx
+++ src/routes/pages/education/education.component.jsx
@@ -1,5 +1,14 @@
+import classes from "./education.module.scss";
+
 const Education = () => {
-  return <h1>It's education page</h1>;
+  return (
+    <div className={classes.education_container}>
+      <div className={classes.sub_container}>
+        <h1>Free University of Tbilisi</h1>
+        <p>Graduated from Free University of Tbilisi</p>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
