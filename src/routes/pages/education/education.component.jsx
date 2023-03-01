efc3a8b119b89b79b06e2579e55858fc0504e501
Ilia Pachulia <ipachulia@evolution.com>
2023-03-02 00:08:58 +0400

    education component is finished

--- src/routes/pages/education/education.component.jsx
+++ src/routes/pages/education/education.component.jsx
@@ -4,8 +4,123 @@ const Education = () => {
   return (
     <div className={classes.education_container}>
       <div className={classes.sub_container}>
-        <h1>Free University of Tbilisi</h1>
-        <p>Graduated from Free University of Tbilisi</p>
+        <h1>
+          <a
+            href="https://freeuni.edu.ge/en/bachelors-in-social-sciences"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Free University of Tbilisi - Governance and Social Sciences
+          </a>
+        </h1>
+        <p>
+          During my time at Free University of Tbilisi, I had the opportunity to
+          study under the Governance and Social Sciences programme. Through this
+          programme, I gained a comprehensive education in a wide range of
+          subjects, including humanities, social and political sciences,
+          economy, law, management, and administration. The knowledge and skills
+          I gained have equipped me with the tools I need to become a
+          critically-minded leader with a strong civic consciousness. I
+          appreciate that the programme's overarching goal is to prepare
+          students for leadership positions in their chosen fields and promote
+          civic engagement. The programme's focus on promoting critical
+          thinking, leadership, and civic engagement aligns with my personal
+          values and goals. Through my studies, I have learned how to become a
+          successful analyst, leader, administrator, and consultant. I am
+          confident that the knowledge and skills I have gained through the
+          Governance and Social Sciences programme at Free University of Tbilisi
+          have prepared me to make a positive impact in my community. I am
+          excited to utilize the tools I have gained to become a leader in my
+          field and contribute to society in a meaningful way.
+        </p>
+      </div>
+      <div className={classes.sub_container}>
+        <h1>
+          <a
+            href="https://www.tbcacademy.ge/frontend-development-angular"
+            target="_blank"
+            rel="noreferrer"
+          >
+            TBC IT Academy - Angular Bootcamp
+          </a>
+        </h1>
+        <p>
+          I had the opportunity to participate in the TBC IT Academy, where I
+          deepened my knowledge of several essential web development tools and
+          frameworks. Through this program, I gained hands-on experience in
+          HTML, CSS, JS, Git, GitHub, NPM, Yarn, SCSS, TypeScript, and Angular.
+          The program's focus on providing practical training in the latest web
+          development technologies allowed me to stay up-to-date with industry
+          trends and best practices. The knowledge I gained during this program
+          has been instrumental in advancing my career as a web developer. I
+          appreciate that the TBC IT Academy is designed to provide students
+          with the skills and knowledge they need to excel in their field. The
+          program's emphasis on hands-on learning allowed me to gain practical
+          experience working with these tools and frameworks, making it easier
+          to apply my skills in real-world scenarios.
+        </p>
+      </div>
+      <div className={classes.sub_container}>
+        <h1>
+          <a
+            href="https://www.udemy.com/course/complete-react-developer-zero-to-mastery/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Zero To Mastery - Complete React Developer in 2023 (w/ Redux, Hooks,
+            GraphQL)
+          </a>
+        </h1>
+        <p>
+          I had the pleasure of completing the Zero To Mastery - Complete React
+          Developer in 2023 (w/ Redux, Hooks, GraphQL) course, which equipped me
+          with the tools and knowledge needed to build enterprise-level React
+          applications and deploy them to production.
+        </p>
+      </div>
+      <div className={classes.sub_container}>
+        <h1>
+          <a
+            href="https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            JavaScript Algorithms and Data Structures Masterclass
+          </a>
+        </h1>
+        <p>
+          Colt Steele's Alogirthms and Data Structures course is a comprehensive
+          resource that allowed me to master dozens of popular algorithms,
+          including 6 sorting algorithms, and implement 10+ data structures from
+          scratch. Through this course, I improved my problem-solving skills and
+          became a stronger developer overall.
+        </p>
+      </div>
+      <div className={classes.sub_container}>
+        <h1>
+          <a
+            href="https://www.udemy.com/course/the-complete-javascript-course/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            The Complete JavaScript Course 2023: From Zero to Expert!
+          </a>
+        </h1>
+        <p>
+          The course has given me a thorough understanding of how JavaScript
+          really works behind the scenes, making me job-ready for any challenges
+          that may arise in my career. Additionally, I have learned how to think
+          and work like a developer, improving my problem-solving skills and
+          workflows. I have gained an in-depth understanding of JavaScript
+          fundamentals, including variables, if/else statements, operators,
+          boolean logic, functions, arrays, objects, loops, and strings.
+          Moreover, the course has provided me with a strong foundation in
+          modern ES6+ concepts like arrow functions, destructuring, spread
+          operators, and optional chaining (ES2020). I now possess knowledge of
+          modern OOP concepts, such as classes, constructors, prototypal
+          inheritance, and encapsulation, as well as complex concepts like the
+          'this' keyword, higher-order functions, and closures.
+        </p>
       </div>
     </div>
   );
